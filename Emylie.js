94fd00167563d769bfd12d9f5b160ae7d3e8e965
gamringer <g.amringer+github@gmail.com>
2014-05-07 11:23:43 -0400

    Fixed scoping in last commit

--- Emylie.js
+++ Emylie.js
@@ -655,10 +655,10 @@ var Emylie = (function(){
 		constructor.prototype.dress = function(dresses){
 			if(dresses == undefined){dresses = constructor.wardrobe;}
 			dresses.forEach((function(dress){
-				this.dom.querySelectorAll('[dress-'+dress+']').forEach(function(el){
+				this.dom.querySelectorAll('[dress-'+dress+']').forEach((function(el){
 					el.scope = this;
 					ns.View.wardrobe[dress].putOn(el);
-				});
+				}).bind(this));
 			}).bind(this));
 		};
 
